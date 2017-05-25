e76070c427ddb02cfd6581186b64c8ee47507552
matt fyke <matt fyke>
2017-05-24 17:21:51 -0700

    task one done

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,43 +3,65 @@ var moment = require("moment");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var burger = require("../models/burger.js");
+var db = require("../models");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
+  // console.log(db.burgers);
+  db.burgers.findAll({}).then(function(data) {
     var hbsObject = {
       burgers: data,
       img: "../public/assets/img/burger.png"
     };
-    console.log(hbsObject);
+    // console.log(hbsObject);
 
     res.render("index", hbsObject);
-    console.log(moment().format("YYYY-MM-DD HH:mm:ss"));
+    // console.log(moment().format("YYYY-MM-DD HH:mm:ss"));
   });
 });
 
 router.post("/", function(req, res) {
-  burger.insertOne([
-    "burger_name", "date"
-  ], [
-    req.body.name, moment().format("YYYY-MM-DD HH:mm:ss")
-  ], function() {
+  db.burgers.create({
+    burger_name: req.body.name,
+    date: moment().format("YYYY-MM-DD HH:mm:ss")
+  }).then(function(data){
     res.redirect("/");
   });
 });
+//     "burger_name", "date"
+//   }
+//   ], [
+//     req.body.name, moment().format("YYYY-MM-DD HH:mm:ss")
+//   ], function() {
+//     res.redirect("/");
+//   });
+// });
 
 router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+  // var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
+  // console.log("condition", condition);
 
-  burger.updateOne({
-    devoured: req.body.devoured
-  }, condition, function() {
-    res.redirect("/");
-  });
+  // burger.updateOne({
+  //   devoured: req.body.devoured
+  // }, condition, function() {
+  //   res.redirect("/");
+  // });
+
+
+  // console.log(req.body.devoured);
+  // console.log(req.params.id);
+  db.burgers.update({
+      devoured: req.body.devoured
+    }, {
+      where: {
+        id: req.params.id
+      }
+    }).then(function(data) {
+      res.redirect("/");
+    });
 });
+
 
 
 
--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name, `date`) VALUES ('Chicken Burger', '2017-05-03 20:10:00');
-INSERT INTO burgers (burger_name, `date`) VALUES ('Double Cheese', '2017-05-03 20:10:00');
-INSERT INTO burgers (burger_name, `date`) VALUES ('Bacon Burger', '2017-05-03 20:10:00');
