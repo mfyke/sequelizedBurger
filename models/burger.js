9905d5d3458a955f3c91af5465e2553ff50b40ce
matt fyke <matt fyke>
2017-05-26 17:02:07 -0700

    stuff

--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,32 @@
-module.exports = function(sequelize, DataTypes) {
-  var burger = sequelize.define("burgers", {
-  	id: {
-  		type: DataTypes.INTEGER,
-  		primaryKey: true
-  	},
-    burger_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    devoured: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    date: DataTypes.DATE
-  });
-  return burger;
+// module.exports = function(sequelize, DataTypes) {
+//   var burger = sequelize.define("burgers", {
+//   	id: {
+//   		type: DataTypes.INTEGER,
+//   		primaryKey: true
+//   	},
+//     burger_name: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//     },
+//     devoured: {
+//       type: DataTypes.BOOLEAN,
+//       defaultValue: false
+//     },
+//     date: DataTypes.DATE
+//   });
+//   return burger;
+// };
+
+module.exports = function(sequelize, Sequelize) {
+ var burger = sequelize.define('burgers', {
+  burger_name: {
+   type: Sequelize.STRING,
+   allowNull: false
+  },
+  devoured: {
+   type: Sequelize.BOOLEAN,
+   defaultValue: false
+  }
+ });
+ return Burger;
 };
